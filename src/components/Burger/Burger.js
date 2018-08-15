ed073b78675fc4070eefb837dfb51a326a2dc50c
alexsaldana9 <alexsaldana9@gmail.com>
2018-08-15 15:11:33 -0400

    preparing for dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,6 +4,11 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+  });
   return (
     <div className={classes.Burger}>
 
